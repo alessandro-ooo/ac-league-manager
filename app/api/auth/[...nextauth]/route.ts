810c2a1cdbb20b27588a14b69f4a7fdb380252fb
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-09-02 23:18:07 +0200

    test again

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import Discord from "next-auth/providers/Discord"
 import prisma from "../../../libs/prismadb"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+
 
 export const authOptions = {
     providers: [
