7346d32feda5709d9427d96cd64d8c4a8773b67f
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-09-03 01:44:26 +0200

    add checkUser, createUser, updateUserName

---
+++ app/libs/prisma/user/functions.ts
@@ -0,0 +1,46 @@
+import { 
+    TAddUser,
+    TCheckUser,
+    TUpdateUser
+} from "./types";
+import prisma from "../../prismadb";
+import { User } from "@prisma/client";
+
+const checkUser = async (params: TCheckUser): Promise<{id: string; name: string;} | null> => {
+    const { id } = params;
+
+    const result = await prisma.user.findUnique({
+        where: {
+            id: id
+        }
+    });
+
+    return result;
+}
+
+const createUser = async (params: TAddUser): Promise<User> => {
+    const {id, name} = params;
+
+    const result = await prisma.user.create({
+        data: {
+            id: id,
+            name: name
+        }
+    });
+
+    return result;
+}
+
+const updateUserName = async (params: TUpdateUser): Promise<void> => {
+    const {to, from} = params; 
+
+    const result = await prisma.user.update({
+        where: {
+            name: from
+        },
+
+        data: {
+            name: to
+        }
+    });
+}
