cadbf304beea4a554ad61bc3cb0d4b51927425bf
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-09-04 21:02:52 +0200

    rework logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,60 @@
-import { getServerSession } from "next-auth"
+import { useSession } from "next-auth/react"
 import { authOptions } from "./api/auth/[...nextauth]/route"
 
 import {LoginBtn, SignOutBtn} from "./components/LoginBtn";
+import { checkUser, createUser } from "./libs/prisma/user/functions";
 import User from "./components/User";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+const Home = async () => {
+    const { data: session } = useSession();
+    const user = await checkUser(session?.user.id);
 
-  return (
-    <div>
-      {
-        session == null && 
+    if(!session) {
+        return (
             <div>
                 Not signed in.
                 <LoginBtn></LoginBtn>
-            </div>          
-      }
-
-      {
-
-        session != null &&
-            <div>
-                <p>{JSON.stringify(session)}</p>
-                <h1>CS rendering</h1>
-                <User />
-                <SignOutBtn></SignOutBtn>
             </div>
-      }
+        )
+    }
 
-    </div>
-  )
+    if(session) {
+        if(user == null) {
+            console.log(session.user.name)
+            const newUser = await createUser(session?.user.id, session?.user.name);
+            console.log(newUser);
+        }
+    }
 }
+
+
+// const Home = async () => {
+//     const session = await getServerSession(authOptions);
+//     const user = await checkUser(session?.user.id);
+
+//     return (
+//         <div>
+//         {
+//             session == null && 
+//                 <div>
+//                     Not signed in.
+//                     <LoginBtn></LoginBtn>
+//                 </div>          
+//         }
+
+//         {
+//             session != null &&
+
+//                 <div>
+//                     <p>{JSON.stringify(session.user.id)}</p>
+//                     <h1>CS rendering</h1>
+//                     <User />
+//                     <SignOutBtn></SignOutBtn>
+//                 </div>
+//         }
+
+//         </div>
+//     )
+// }
+
+export default Home
