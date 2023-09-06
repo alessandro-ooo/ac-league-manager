32681a5f2c33563c84a5fa2e82ed1eef42d2d583
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-09-06 18:29:46 +0200

    enable experimental feature serverAction

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {}
 
 module.exports = {
     ...nextConfig,
+    experimental: {
+        serverActions: true,
+    },
     env: {
         DISCORD_ID: process.env.DISCORD_ID,
         DISCORD_SECRET: process.env.DISCORD_SECRET
