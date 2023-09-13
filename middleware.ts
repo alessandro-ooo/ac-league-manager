b4d948aa588b881d5af272d577db6a768bfc4674
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-09-13 15:48:04 +0200

    add redirects in middleware

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,19 @@ import {
 } from 'next/server'
 
 const middleware = async (request: NextRequest) => {
+    const cookie = request.cookies.get('username');
 
+    if(request.nextUrl.pathname.startsWith('/dashboard')) {
+        if(cookie === undefined) {
+            return NextResponse.redirect(new URL('/login', request.url));
+        }
+    }
+
+    if(request.nextUrl.pathname.startsWith('/login')) {
+        if(cookie != undefined) {
+            return NextResponse.redirect(new URL('/dashboard', request.url));
+        }
+    }
 }
 
 export default middleware
-// export const config = { matcher: ["/dashboard"] }
-// export { default } from "next-auth/middleware"
