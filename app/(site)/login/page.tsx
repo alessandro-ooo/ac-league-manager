6a0e3b1e2ce66fb680d16b236bff1cb44e3a0009
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-09-13 15:42:34 +0200

    send user to dashboard if logged in

--- app/(site)/login/page.tsx
+++ app/(site)/login/page.tsx
@@ -1,8 +1,14 @@
-"use client"
 import { LoginBtn } from "@/app/components/LoginBtn"
+import { redirect } from "next/navigation";
+import { cookies } from 'next/headers'
 
 const Login = () => {
+    const usernameCookie = cookies().get('username')
 
+    if(usernameCookie != undefined) {
+        return redirect('/dashboard');
+    }
+    
     return (
         <p>login here <LoginBtn /></p>
     )
