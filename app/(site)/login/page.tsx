878d776dc7fe6e87f391cd51718ec032f61170ab
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-09-13 15:47:45 +0200

    remove redirect

--- app/(site)/login/page.tsx
+++ app/(site)/login/page.tsx
@@ -1,13 +1,6 @@
 import { LoginBtn } from "@/app/components/LoginBtn"
-import { redirect } from "next/navigation";
-import { cookies } from 'next/headers'
 
 const Login = () => {
-    const usernameCookie = cookies().get('username')
-
-    if(usernameCookie != undefined) {
-        return redirect('/dashboard');
-    }
     
     return (
         <p>login here <LoginBtn /></p>
