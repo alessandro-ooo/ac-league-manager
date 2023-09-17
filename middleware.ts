4db9aaf273b74b4ef6ec7fa32eb466fc3f78115c
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-09-17 20:46:13 +0200

    send user to new page to create his display name

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,23 @@ import {
 } from 'next/server'
 
 const middleware = async (request: NextRequest) => {
+    console.log(request.url)
     const cookie = request.cookies.get('username');
+    const discordid = request.cookies.get('discordid');
 
-    if(request.nextUrl.pathname.startsWith('/dashboard')) {
+    if(request.nextUrl.pathname === '/dashboard') {
         if(cookie === undefined) {
-            return NextResponse.redirect(new URL('/login', request.url));
+            return NextResponse.redirect(new URL('/login/new', request.url));
         }
     }
 
-    if(request.nextUrl.pathname.startsWith('/login')) {
+    if(request.nextUrl.pathname === '/login') {
         if(cookie != undefined) {
             return NextResponse.redirect(new URL('/dashboard', request.url));
         }
+        if(discordid != undefined && cookie === undefined) {
+            return NextResponse.redirect(new URL('/login/new', request.url));
+        }
     }
 }
-
 export default middleware
