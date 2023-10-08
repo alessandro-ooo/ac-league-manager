a124d54dc6bb7ceb67b9bed6a2e73f45c0f7b778
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-10-08 19:52:54 +0200

    show liveries to user

--- app/components/dashboard/Vehicles.tsx
+++ app/components/dashboard/Vehicles.tsx
@@ -1,15 +1,34 @@
-import { TVehiclesProps } from "../types";
+import { getCarLiveries } from "@/app/libs/prisma/car/cars";
+import { TLiveriesProps, TVehiclesProps } from "../types";
 
-const Vehicles = (props: TVehiclesProps) => {
+
+const Vehicles = async (props: TVehiclesProps) => {
     const { data } = props;
 
     return (
-        <div>
+        <div className="flex flex-col">
             {data.map((item, i) => {
-                return <a id={item.cid.toString()}>{item.name}</a>
+                return (
+                    <div>
+                        <a className="hover:bg-black hover:text-white" id={item.cid.toString()}>{item.name}</a>
+                        <Liveries data={item.liveries} />
+                    </div>
+                )
+            })}
+
+        </div>
+    )
+}
+
+const Liveries = async (props: TLiveriesProps) => {
+    const { data } = props;
+    return (
+        <div className="flex flex-col">
+            {data.map((item, i) => {
+                return <a className="hover:bg-black hover:text-white">{item.name}</a>
             })}
         </div>
     )
 }
 
-export default Vehicles;
+export default Vehicles
