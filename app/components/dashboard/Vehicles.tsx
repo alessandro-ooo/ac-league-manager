881c43d638f88ff2d501f9595ccf7aecf3d350c3
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-10-10 22:42:43 +0200

    add preview images for the liveries

--- app/components/dashboard/Vehicles.tsx
+++ app/components/dashboard/Vehicles.tsx
@@ -1,6 +1,6 @@
 import { getCarLiveries } from "@/app/libs/prisma/car/cars";
 import { TLiveriesProps, TVehiclesProps } from "../types";
-
+import Image from 'next/image'
 
 const Vehicles = async (props: TVehiclesProps) => {
     const { data } = props;
@@ -9,13 +9,15 @@ const Vehicles = async (props: TVehiclesProps) => {
         <div className="flex flex-col">
             {data.map((item, i) => {
                 return (
-                    <div>
+                    <div className="flex flex-col" key={i.toString()}>
                         <a className="hover:bg-black hover:text-white" id={item.cid.toString()}>{item.name}</a>
-                        <Liveries data={item.liveries} />
+                        <div className="">
+                            <button>create livery</button>
+                            <Liveries data={item.liveries} />                     
+                        </div>
                     </div>
                 )
             })}
-
         </div>
     )
 }
@@ -25,7 +27,12 @@ const Liveries = async (props: TLiveriesProps) => {
     return (
         <div className="flex flex-col">
             {data.map((item, i) => {
-                return <a className="hover:bg-black hover:text-white">{item.name}</a>
+                return (
+                    <>
+                        <a className="hover:bg-black hover:text-white">{item.name}</a>
+                        <Image width={100} height={100} src={item.texture} alt="hi"/>  
+                    </>
+                )
             })}
         </div>
     )
