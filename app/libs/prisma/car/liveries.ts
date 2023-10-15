d121ff203d36f69ab84cfe7211f34035fcc25bfa
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-10-15 22:07:37 +0200

    test file

--- app/libs/prisma/car/liveries.ts
+++ app/libs/prisma/car/liveries.ts
@@ -1,2 +1,6 @@
 import prisma from "../../prismadb";
 
+{"preview1": "/models/03_toyo_84/preview.jpg",
+"preview2": "/models/03_toyo_84/preview2.jpg"
+"preview3": "/models/03_toyo_84/preview.jpg"
+["/models/03_toyo_84/preview.jpg", "/models/03_toyo_84/preview2.jpg", "/models/03_toyo_84/preview3.jpg"]}
