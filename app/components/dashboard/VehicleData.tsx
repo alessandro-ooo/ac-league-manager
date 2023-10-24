37b3c59909f91009d7341fdef51ac8c1c2d16b86
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-10-24 22:56:13 +0200

    remove comments

--- app/components/dashboard/VehicleData.tsx
+++ app/components/dashboard/VehicleData.tsx
@@ -4,8 +4,8 @@ import { TVehicleDataProps } from "../types"
 
 const VehicleData = (props: TVehicleDataProps) => {
     const router = useRouter();
-    const {cid, name} = props;
-    return <div onClick={(cid) => router.push('/dashboard/' + cid.currentTarget.id)} className="hover:bg-black hover:text-white" id={cid}>{name}</div>
+    const {lid, name} = props;
+    return <div onClick={(cid) => router.push('/dashboard/' + cid.currentTarget.id)} className="hover:bg-black hover:text-white" id={lid.toString()}>{name}</div>
 }
 
 export default VehicleData;
--- app/components/dashboard/Vehicles.tsx
+++ app/components/dashboard/Vehicles.tsx
@@ -1,6 +1,5 @@
 
 import { TLiveriesProps, TVehiclesProps } from "../types";
-import Image from 'next/image'
 import VehicleData from "./VehicleData";
 
 const Vehicles = (props: TVehiclesProps) => {
@@ -30,20 +29,9 @@ const Liveries = async (props: TLiveriesProps) => {
     return (
         <div className="flex flex-col">
             {data.map((item, i) => {
-                // const previews = JSON.parse(item.preview);
                 return (
                     <div>
-                        <VehicleData cid="1" name={item.name}/>
-                        {/* <a className="hover:bg-black hover:text-white">{item.name}</a> */}
-                        {/* <VehicleData cid="1" name={item-na} /> */}
-                        {/* {previews.map((previewImage: string, p: any) => {
-                            return (
-                                <div>
-                                    <p>{previewImage}</p>
-                                    <Image width={100} height={100} src={previewImage} alt="hi"/>
-                                </div>
-                            )
-                        })} */}
+                        <VehicleData lid={item.lid} name={item.name}/>
                     </div>
                 )
             })}
