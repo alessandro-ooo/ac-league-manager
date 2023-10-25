87e3727575b08b83918887639ce6e99334594cd8
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-10-25 23:54:23 +0200

    add Settings with dynamic routes

---
+++ app/(site)/settings/[[...id]]/page.tsx
@@ -0,0 +1,15 @@
+import UserSettingsForm from "@/app/components/forms/UserSettingsForm";
+import { checkUser } from "@/app/libs/prisma/user/functions"
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { cookies } from "next/headers";
+
+const Settings = async ({ params }: { params: { id: string } }) => {
+    // const discordId: RequestCookie | any = cookies().get('discordid');
+    const userData = await checkUser(params.id.toString());
+
+    return (
+        <UserSettingsForm name={userData!.name} steamid={userData!.steamid} id={params.id} />
+    )
+}
+
+export default Settings
