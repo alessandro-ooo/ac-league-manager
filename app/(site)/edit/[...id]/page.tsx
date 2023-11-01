1f22b9291f4d52b3a4ebcc701cf65c4efe1288fb
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-11-01 17:49:50 +0100

    redirect user to settings page

--- app/(site)/edit/[...id]/page.tsx
+++ app/(site)/edit/[...id]/page.tsx
@@ -1,7 +1,18 @@
 import { getSteamID } from "@/app/libs/prisma/user/functions"
 import { cookies } from "next/headers"
+import { redirect } from 'next/navigation'
 
-const Edit = ({ params }: { params: { lid: string } }) => {
+const Edit = async ({ params }: { params: { lid: string } }) => {
     const discordid = cookies().get('discordid');
-    const userSteamID = getSteamID(discordid?.value as string) 
+    const userSteamID = await getSteamID(discordid?.value as string) ;
+
+    if(userSteamID == 0) {
+        return redirect('/settings/' + discordid?.value);
+    }
+
+    return (
+        <p>edit livery page</p>
+    )
 }
+
+export default Edit
