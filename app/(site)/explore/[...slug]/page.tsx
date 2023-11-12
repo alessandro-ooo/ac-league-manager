acf7e99e2f77dbdd2eb4041aa3a894550d709f78
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-11-12 17:06:08 +0100

    add /explore page with slugs

--- app/(site)/explore/[...slug]/page.tsx
+++ app/(site)/explore/[...slug]/page.tsx
@@ -1,6 +1,24 @@
-const Explore = async ({ params }: { params: { lid: string } }) => {
+import VehicleData from "@/app/components/dashboard/VehicleData";
+import FiltersForm from "@/app/components/forms/FiltersForm";
 
-    return console.log(params)
+import { filter } from "@/app/libs/prisma/car/cars"
+
+const Explore = async ({ params }: { params: { slug: string } }) => {
+    const livery = await filter(params.slug[0], params.slug[1]);
+    return (
+        <div>
+            <FiltersForm />
+
+            {livery.map((item, i) => {
+
+                return (
+                    <div>
+                        <VehicleData lid={item.lid} name={item.name} />                        
+                   </div>
+                )
+            })}
+        </div>
+    )
 }
 
 
