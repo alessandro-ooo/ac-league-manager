32519ab5e2be327eac1884267ca38d81c1a399eb
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-12-03 19:22:50 +0100

    add type for server cfg

--- Web/app/components/types.ts
+++ Web/app/components/types.ts
@@ -16,6 +16,44 @@ type TNewUserForm = {
     id: string,
 }
 
+type TServerSettingsForm = {
+    name: string,
+    cars: string,
+    config_track: string,
+    track: string,
+    sun_angle: number,
+    password: string,
+    admin_password: string,
+    udp_port: number,
+    tcp_port: number,
+    http_port: number,
+    pickup_mode_enabled: number,
+    loop_mode: number,
+    sleep_time: number,
+    client_send_interval_hz: number,
+    send_buffer_size: number,
+    recv_buffer_size: number,
+    race_over_time: number,
+    kick_quorum: number,
+    vote_duration: number,
+    blacklist_mode: number,
+    fuel_rate: number,
+    damage_multiplier: number,
+    tyre_wear_rate: number,
+    abs_allowed: number,
+    tc_allowed: number,
+    stability_allowed: number,
+    autoclutch_allowed: number,
+    tyre_blankets_allowed: number,
+    force_virtual_mirror: number,
+    register_to_lobby: number,
+    max_clients: number,
+    udp_plugin_local_port: number,
+    udp_plugin_address: string,
+    auth_plugin_address: string,
+    legal_tyres: string
+}
+
 type TFiltersForm = {
     carName: string
     author?: string
@@ -72,6 +110,7 @@ export type {
     TDialog,
     TInputProps,
     TNewUserForm,
+    TServerSettingsForm,
     TFiltersForm,
     TModelProps,
     TVehicleDataProps,
