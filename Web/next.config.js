97196ff2ce75f223125b773dff236955d322a908
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-12-06 16:06:22 +0100

    remove instrumentation

--- Web/next.config.js
+++ Web/next.config.js
@@ -5,7 +5,6 @@ module.exports = {
     ...nextConfig,
     experimental: {
         serverActions: true,
-        instrumentationHook: true,
     },
     // env: {
     //     DISCORD_ID: process.env.DISCORD_ID,
