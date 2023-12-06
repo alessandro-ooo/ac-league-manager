2e62c9e7ce59854c8d07bba9ea488a55e998ab62
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-12-06 16:07:21 +0100

    add some space because yes

--- Web/app/components/Input.tsx
+++ Web/app/components/Input.tsx
@@ -4,7 +4,7 @@ import React, { LegacyRef } from "react";
 const Input = React.forwardRef(({label, type, placeholder, ...rest} : TInputProps, ref: LegacyRef<HTMLInputElement>) => {
 
     return (
-        <div>
+        <div className="space-x-24">
             <label>{label}</label>
             <input type={type} placeholder={placeholder} ref={ref} {...rest}/> 
         </div>
