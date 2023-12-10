f97aafde2f0c856f220fe21be06e948d5e9328b2
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-12-10 15:37:50 +0100

    create checkCFG, inject, updateCFG

--- Web/app/libs/prisma/cfg/functions.ts
+++ Web/app/libs/prisma/cfg/functions.ts
@@ -1,24 +1,78 @@
-import { ServerCFG } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import prisma from "../../prismadb";
+import { TServerSettings } from "@/app/components/types";
 
-const arrServer = [
+const updateCFG = async (data: any) => {
 
-]
+    for (const key in data) {
+        const keyValue = data[key];
+        
+        const upd = await prisma.serverCFG.update({ 
+            where: { field: key },
+            data: {value: keyValue}
+        });
+    }
+}
 
-// const check = async (): Promise<number> => {
-//     const count = await prisma.serverCFG.count();
-//     return count;
-// }
+const getAllFields = async () => {
+    const result = await prisma.serverCFG.findMany();
+    return result;
+}
 
-// const inject = async (): Promise<ServerCFG> => {
+const checkCFG = async (): Promise<number> => {
+    const count = await prisma.serverCFG.count();
+    return count;
+}
 
-//     const result = await prisma.serverCFG.createMany({
-//         data: {
+const inject = async (): Promise<Prisma.BatchPayload> => {
+    console.log("- warn - Injecting ServerCFG table with server data.");
+    const result = await prisma.serverCFG.createMany({
+        data: [
+            { field: 'NAME', value: 'AC_Server' },
+            { field: 'CARS', value: 'bmw_m3_e30' },
+            { field: 'CONFIG_TRACK', value: '' },
+            { field: 'TRACK', value: 'magione' },
+            { field: 'SUN_ANGLE', value: '48' },
+            { field: 'PASSWORD', value: 'CHANGEME' },
+            { field: 'ADMIN_PASSWORD', value: '' },
+            { field: 'UDP_PORT', value: '9600' },
+            { field: 'TCP_PORT', value: '9600' },
+            { field: 'HTTP_PORT', value: '8081' },
+            { field: 'PICKUP_MODE_ENABLED', value: '1' },
+            { field: 'LOOP_MODE', value: '1' },
+            { field: 'SLEEP_TIME', value: '1' },
+            { field: 'CLIENT_SEND_INTERVAL_HZ', value: '18' },
+            { field: 'SEND_BUFFER_SIZE', value: '0' },
+            { field: 'RECV_BUFFER_SIZE', value: '0' },
+            { field: 'RACE_OVER_TIME', value: '180' },
+            { field: 'KICK_QUORUM', value: '85' },
+            { field: 'VOTING_QUORUM', value: '80' },
+            { field: 'VOTE_DURATION', value: '20' },
+            { field: 'BLACKLIST_MODE', value: '1' },
+            { field: 'FUEL_RATE', value: '100' },
+            { field: 'DAMAGE_MULTIPLIER', value: '100' },
+            { field: 'TYRE_WEAR_RATE', value: '100' },
+            { field: 'ALLOWED_TYRES_OUT', value: '2' },
+            { field: 'ABS_ALLOWED', value: '1' },
+            { field: 'TC_ALLOWED', value: '1' },
+            { field: 'STABILITY_ALLOWED', value: '0' },
+            { field: 'AUTOCLUTCH_ALLOWED', value: '0' },
+            { field: 'TYRE_BLANKETS_ALLOWED', value: '0' },
+            { field: 'FORCE_VIRTUAL_MIRROR', value: '1' },
+            { field: 'REGISTER_TO_LOBBY', value: '1' },
+            { field: 'MAX_CLIENTS', value: '18' },
+            { field: 'UDP_PLUGIN_LOCAL_PORT', value: '0' },
+            { field: 'UDP_PLUGIN_ADDRESS', value: '' },
+            { field: 'AUTH_PLUGIN_ADDRESS', value: '' },
+            { field: 'LEGAL_TYRES', value: 'SV' }
+        ]
+    });
+    return result;
+}
 
-//         }
-//     });
-// }
-
-// export {
-//     check
-// }
+export {
+    checkCFG,
+    inject,
+    getAllFields,
+    updateCFG
+}
