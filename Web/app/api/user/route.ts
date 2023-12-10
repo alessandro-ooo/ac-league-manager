e42eeb71071aaaa2f9b5e0385ebca6a135f130ad
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-12-10 15:36:35 +0100

    server now checks if there are settings in the database

--- Web/app/api/user/route.ts
+++ Web/app/api/user/route.ts
@@ -1,8 +1,8 @@
+import { checkCFG, inject } from "@/app/libs/prisma/cfg/functions";
 import { 
     createUser, userCount 
 } from "@/app/libs/prisma/user/functions";
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 import { 
     NextRequest, 
@@ -15,9 +15,16 @@ export async function POST (request: NextRequest) {
     const user = await createUser(data.id, data.username, (count == 0 ? 2 : 0));
     cookies().set('admin', (count == 0 ? "2" : "0"));
 
+    if(count == 0) {
+        const exists: number = await checkCFG();
+        if(exists == 0) {
+            console.log(" - warn - no server settings found.");
+            inject();
+        }
+    }
+
     if(user) {
         cookies().set('username', user.name);
-        
         return NextResponse.json({message: "OK", status: 200});
     }
     return NextResponse.json({message: "nothing happened", status: 204});
