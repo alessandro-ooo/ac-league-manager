cc70c13db6e9479a5c8abce0b8807687712deab1
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-01-10 15:34:15 +0100

    sync laps with AC server

--- Web/app/libs/file/functions.ts
+++ Web/app/libs/file/functions.ts
@@ -1,10 +1,11 @@
 import { TDrivers } from "@/app/components/types";
-import { getAllFields, getRaceDrivers } from "../prisma/cfg/functions"
+import { getAllFields, getRace, getRaceDrivers } from "../prisma/cfg/functions"
 import { Mode, ObjectEncodingOptions, OpenMode, promises as fs } from 'fs';
 import { Abortable } from "node:events";
 
 const createINI = async (race: number) => {
     let entry_list_str: string = "";
+    const raceData = await getRace(race);
     const drivers = await getRaceDrivers(race);
 
     for(let i = 0; i < drivers.length; i++) {
@@ -23,7 +24,7 @@ const createINI = async (race: number) => {
 
     const server_cfg_practice = "\n[PRACTICE]\nNAME=Practice\nTIME=10\nIS_OPEN=1\n";
     const server_cfg_qualify = "\n[QUALIFY]\nNAME=Qualify\nTIME=10\nIS_OPEN=1\n";
-    const server_cfg_race = "\n[RACE]\nNAME=Race\nLAPS=5\nWAIT_TIME=60\nIS_OPEN=1";
+    const server_cfg_race = "\n[RACE]\nNAME=Race\nLAPS=" + raceData?.laps + "\nWAIT_TIME=60\nIS_OPEN=1\n";
     const server_cfg_weather="\n[WEATHER_0]\nGRAPHICS=3_clear\nBASE_TEMPERATURE_AMBIENT=18\nBASE_TEMPERATURE_ROAD=6\nVARIATION_AMBIENT=1\nVARIATION_ROAD=1\n"
 
     server_cfg+= server_cfg_practice + server_cfg_qualify + server_cfg_race + server_cfg_weather;
