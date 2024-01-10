d4fb1b1cf3a933439f519ee6f8ccd62d947eb083
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-01-10 15:33:59 +0100

    add button to delete server for admins

--- Web/app/components/race/race.tsx
+++ Web/app/components/race/race.tsx
@@ -2,9 +2,18 @@
 import { getAdminCookie } from "@/app/libs/cookies/functions";
 import { TRaceProps } from "../types"
 import { createINI } from "@/app/libs/file/functions";
+import { useRouter } from 'next/navigation'
 
-const clickButton = async (id: number) => {
+const startServer = async (id: number) => {
     const res = await fetch('/api/executeACServer', {
+        method: 'POST',
+        body: JSON.stringify(id)
+    });
+    return;
+} 
+
+const deleteServer = async (id: number) => {
+    const res = await fetch('/api/deleteRace', {
         method: 'POST',
         body: JSON.stringify(id)
     });
@@ -16,6 +25,7 @@ const Race = (props: TRaceProps) => {
     const {data} = props;
     // const isAdmin = getAdminCookie();
     const isAdmin = 2;
+    const router = useRouter();
 
     return (
         <div>
@@ -23,12 +33,16 @@ const Race = (props: TRaceProps) => {
                 return (
                     <div key={item.id}>
                         <p>{item.race}</p>
+                        <div>
+                            <button onClick={() => startServer(item.id)}>Sign in</button>
                         {Number(isAdmin) > 0 &&
                             <div>
-                                <button onClick={() => clickButton(item.id)}>Start server</button>
-                                <button onClick={() => clickButton(item.id)}>Edit server</button>
+                                <button onClick={() => startServer(item.id)}>Start server</button>
+                                <button onClick={() => router.push('/admin/editrace/' + item.id)}>Edit server</button>
+                                <button onClick={() => deleteServer(item.id)}>Delete server</button>
                             </div>
                         }
+                        </div>
                     </div>
                 )
             })}
